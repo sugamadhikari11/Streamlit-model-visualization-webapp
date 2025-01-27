717ad27b433fc8027f4c3b3e6159a80c144bf870
sugamadhikari11 <sugam.19217113@gmail.com>
2025-01-27 23:59:35 +0545

    initial setup done

---
+++ app.py
@@ -0,0 +1,23 @@
+import streamlit as st
+
+# Set the title of the app
+st.set_page_config(page_title="Big Mart Sales Prediction", layout="wide")
+
+# Create a sidebar for navigation
+st.sidebar.title("Navigation")
+page = st.sidebar.radio("Go to", ["Welcome", "Prediction"])
+
+# Welcome Page
+if page == "Welcome":
+    st.title("Welcome to Big Mart Sales Prediction")
+    st.write("""
+        This application allows you to predict sales based on various input features.
+        Use the navigation bar to explore the application.
+    """)
+    st.image("https://via.placeholder.com/800x400", caption="Welcome to Big Mart Sales Prediction")  # Placeholder image
+
+# Prediction Page
+elif page == "Prediction":
+    st.title("Sales Prediction")
+    st.write("This section will contain the sales prediction functionality.")
+    st.write("Currently, the prediction feature is not available as we don't have data.")
